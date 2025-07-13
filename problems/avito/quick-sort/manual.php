2fd2a881f36d35ba6ca252cdf6ba16f080960524
dusk <dusk1986@gmail.com>
2025-07-13 17:20:25 +0300

    #master

---
+++ problems/avito/quick-sort/manual.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @param array<int> $array
+ * @return int[]
+ */
+function quicksort(array $array): array
+{
+    if (count($array) <= 1) {
+        return $array;
+    }
+
+    $pivot = $array[0];
+    $left = $right = [];
+
+    for ($i = 1; $i < count($array); $i++) {
+        if ($array[$i] < $pivot) {
+            $left[] = $array[$i];
+        } else {
+            $right[] = $array[$i];
+        }
+    }
+
+    return array_merge(quicksort($left), [$pivot], quicksort($right));
+}
+
+// Пример использования
+$array = [5, 3, 8, 4, 2];
+echo "Исходный массив: ";
+xdebug_var_dump($array);
+
+$sortedArray = quicksort($array);
+echo "Отсортированный массив: ";
+xdebug_var_dump($sortedArray);
